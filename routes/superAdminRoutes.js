f5ca946884b3fd13eb3ded8cd24951b03795cddc
methy-tec <pmethychella@gmail.com>
2025-10-02 08:39:18 +0100

    Update superAdminRoutes.js

--- routes/superAdminRoutes.js
+++ routes/superAdminRoutes.js
@@ -1,19 +1,20 @@
 import express from "express";
 import { register, login, refreshToken, getStatistics } from "../controllers/superAdminController.js";
-import { Sequelize } from "sequelize";
 import { verifyToken, verifyRole } from "../middlewares/authMiddlewares.js";
 
 const router = express.Router();
 
+// Routes superadmin
+router.post("/register", register);
+router.post("/login", login);
+router.post("/refresh", refreshToken);
+
+// Route protégée test
 router.get("/only", verifyToken, verifyRole("superadmin"), (req, res) => {
-  res.json({ message: "Bienvenue Admin ✅" });
+  res.json({ message: "Bienvenue SuperAdmin ✅" });
 });
 
-router.post('/register', register);
-router.post('/login', login);
-router.post("/refresh", refreshToken);
+// Statistiques
 router.get("/statistics", verifyToken, verifyRole("superadmin"), getStatistics);
-
-
 
 export default router;
