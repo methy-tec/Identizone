ce6c6a01415e9444767f62bc7766b8af14ef7751
methy-tec <pmethychella@gmail.com>
2025-10-02 08:35:05 +0100

    Update superAdminRoutes.js

--- routes/superAdminRoutes.js
+++ routes/superAdminRoutes.js
@@ -12,7 +12,7 @@ router.get("/only", verifyToken, verifyRole("superadmin"), (req, res) => {
 router.post('/register', register);
 router.post('/login', login);
 router.post("/refresh", refreshToken);
-router.get("/statistics", verifyToken, verifyRole["superadmin"], getStatistics);
+router.get("/statistics", verifyToken, verifyRole("superadmin"), getStatistics);
 
 
 
