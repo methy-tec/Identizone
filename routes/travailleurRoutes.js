a780476e6bad24f256c33550bfb7084af4cf36db
methy-tec <pmethychella@gmail.com>
2025-10-09 01:01:58 +0100

    Update travailleurRoutes.js

--- routes/travailleurRoutes.js
+++ routes/travailleurRoutes.js
@@ -5,7 +5,7 @@ import { verifyRole, verifyToken } from "../middlewares/authMiddlewares.js";
 
 const router = express.Router();
 
-router.post("/", verifyToken, verifyRole("admin", "preadmin"), upload.single("photo"), createTravailler);
+router.post("/", verifyToken, verifyRole("preadmin"), upload.single("photo"), createTravailler);
 router.post("/login", login);
 router.get("/list", verifyToken, verifyRole("superadmin", "admin", "preadmin"), getAllTravailleurs);
 router.get("/:id", verifyToken, getTravailleurById);
