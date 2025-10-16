809f879c845bcd99d1de1dfdf3ff2dde89a0f768
methy-tec <pmethychella@gmail.com>
2025-10-16 04:49:34 +0100

    Update adminRoute.js

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register, login, getAllAdmins, updateAdmin, getAdminById, refreshToken, getStatistics } from "../controllers/adminController.js";
+import { register, login, getAllAdmins, updateAdmin, getAdminById, refreshToken, getStatistics, meConnect } from "../controllers/adminController.js";
 import upload from "../middlewares/multerMiddlewares.js";
 import { verifyRole, verifyToken } from "../middlewares/authMiddlewares.js";
 
@@ -13,6 +13,7 @@ router.get("/statistics", verifyToken, verifyRole("admin", "preadmin"), getStati
 router.post("/", verifyToken, verifyRole("superadmin"), upload.single("photo"), register);
 router.post("/login", login);
 router.get("/list", verifyToken, verifyRole("superadmin"), getAllAdmins);
+router.get("/me", verifyToken, meConnect);
 router.put("/:id", upload.single("photo"), updateAdmin);
 router.get("/:id", getAdminById);
 router.post("/refresh", refreshToken);
