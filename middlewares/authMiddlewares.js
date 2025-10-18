060c5a643920a5056eb7063a0d042a135daa4b23
methy-tec <pmethychella@gmail.com>
2025-10-18 06:13:56 +0100

    Update authMiddlewares.js

--- middlewares/authMiddlewares.js
+++ middlewares/authMiddlewares.js
@@ -1,24 +1,26 @@
 // middlewares/verifyToken.js
 import jwt from "jsonwebtoken";
 
+
 export const verifyToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
-  console.log("Authorization header:", authHeader); 
-  
+  console.log("Authorization header:", authHeader);
+
   const token = authHeader?.split(" ")[1]; // format attendu: Bearer <token>
-  console.log("Token décodé:", req.user);
   
   if (!token) {
+    console.log("❌ Aucun token trouvé");
     return res.status(401).json({ message: "⛔ Token manquant" });
   }
-
-
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded; // on stocke { id, role } dans req
+    console.log("✅ Token décodé:", decoded);
+
     next();
   } catch (err) {
+    console.log("❌ Erreur de vérification du token:", err.message);
     return res.status(401).json({ message: "⛔ Token invalide" });
   }
 };
